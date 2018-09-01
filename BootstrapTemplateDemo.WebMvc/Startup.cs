0c4bd39fcd450700a4d393b9f289555d2daeb377
Paul O'Connor <jamespauloconnor@gmail.com>
2018-09-01 14:58:23 -0400

    Added gitignore

---
+++ BootstrapTemplateDemo.WebMvc/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(BootstrapTemplateDemo.WebMvc.Startup))]
+namespace BootstrapTemplateDemo.WebMvc
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
